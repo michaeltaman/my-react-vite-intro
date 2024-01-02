3c02355b5155b8882bf668edce9620b9daf6dda7
michaeltaman <42732094+michaeltaman@users.noreply.github.com>
2024-01-02 12:40:33 +0200

    Button styles DifferencesSection for mobile attempt-3

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,26 +1,21 @@
 import PropTypes from 'prop-types';
-import classes from './Button.module.css';
+import styles from './Button.module.css';
 
-//console.log(classes)
-
-export default function Button({children, isActive, ...props}) {
-    //console.log('Button' ,isActive)
-    return (
-
-        <button
-        {...props}
-         className={
-            isActive ? `${classes.button} ${classes.active}` : classes.button
-        }>
-            {children}
-        </button>
-    );
+function Button({ children, isActive, ...props }) {
+  return (
+    <button
+      {...props}
+      className={isActive ? `${styles.button} ${styles.active}` : styles.button}
+    >
+      {children}
+    </button>
+  );
 }
+
+export default Button;
 
 Button.propTypes = {
     children: PropTypes.node,
     onClick: PropTypes.func,
     isActive: PropTypes.bool,
 }
-
-
